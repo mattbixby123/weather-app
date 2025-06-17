9fa2a707fda6c0cdc2acf7d8d4ba71945bfc279a
mattbixby123 <matthew.bixby1@gmail.com>
2025-06-17 14:07:27 -0400

    notes added to root page for api call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,64 @@
 import Navbar from "./components/Navbar";
 
+// https://api.openweathermap.org/data/2.5/forecast?q={location}&appid={API_KEY} - f0rcast by location
+
+// forcast call by lat / lon- https://api.openweathermap.org/data/2.5/forecast?lat=40.6501&lon=-73.9496&appid={API_KEY}
+
+interface WeatherDetail {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherDetail[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
